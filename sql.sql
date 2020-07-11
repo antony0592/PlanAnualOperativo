79bf3d95c38e540c547a8057163aac232c3b366a
nasg2013 <62192753+nasg2013@users.noreply.github.com>
2020-07-11 10:43:43 -0600

    SQL

---
+++ sql.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE PlanAnualOperativoAntony2020
+GO
+CREATE TABLE PAO_EjeTematico (
+	IdEjeTematico int identity (1,1) Primary key,
+	EjTematico varchar(45)	
+);
+GO
+CREATE TABLE PAO_Objetivo(
+	IdObjetivo int identity(1,1) primary key,
+	IdTematico int,
+	Anno int,
+	Oficina varchar(45),
+	TemaEstretegico varchar(100),
+	Objetivo varchar(500)
+);
+GO
+ALTER TABLE PAO_Objetivo
+ADD FOREIGN KEY (IdTematico)
+REFERENCES PAO_EjeTematico(IdEjeTematico);
+GO
+CREATE TABLE PAO_Coordinacion(
+	IdCoordinacion int identity(1,1) Primary Key,
+	IdObjetivo int,
+	TipoOficina varchar(45),
+	NombreOficina varchar(200)
+);
+ALTER TABLE PAO_Coordinacion
+ADD CHECK (TipoOficina='INTERNA' OR TipoOficina='EXTERNA');
+GO
+ALTER TABLE PAO_Coordinacion
+ADD FOREIGN KEY (IdObjetivo)
+REFERENCES PAO_Objetivo(IdObjetivo);
+GO
+CREATE TABLE PAO_Meta(
+	IdMeta int identity (1,1) Primary Key,
+	IdObjetivo int,
+	FechaEI VARCHAR(64),
+	FechaEF VARCHAR(64),
+	ValorInicial int,
+	ValorEsperado int,
+	Indicador int,
+	Formula varchar(100),
+	TipoControl varchar(45),
+	Cantidad int,
+	Medida varchar(50)
+);
+GO
+ALTER TABLE PAO_Meta
+ADD FOREIGN KEY (IdObjetivo)
+REFERENCES PAO_Objetivo(IdObjetivo);
+GO
+CREATE TABLE PAO_Actividad(
+	IdActividad int identity(1,1) Primary Key,
+	IdMeta int,
+	IdAvance int,
+	Actividad varchar(50),
+	DetalleActividad varchar(500)	
+);
+GO
+ALTER TABLE PAO_Actividad
+ADD FOREIGN KEY (IdMeta)
+REFERENCES PAO_Meta(IdMeta);
+GO
+CREATE TABLE PAO_Avance(
+	IdAvance INT IDENTITY(1,1) PRIMARY KEY,
+	Oficina varchar(50),
+	Objetivo varchar(500),
+	Comentario varchar(500),
+	Anno int,
+	FechaActividad varchar(64),
+	FileUrl varchar(500),
+	Terminado int 
+);
+GO
+ALTER TABLE PAO_Actividad
+ADD FOREIGN KEY (IdAvance)
+REFERENCES PAO_Avance(IdAvance);
+GO
+ALTER TABLE PAO_Avance
+ADD CHECK (Terminado='1' OR Terminado='0');
+
